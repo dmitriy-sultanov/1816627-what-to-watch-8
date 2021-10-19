6c7575df7bcbceef767663c2e2df2e0c71ec89e9
Дима <sultanovdu@gmail.com>
2021-10-19 11:54:53 +0300

    Отрисовка карточек, добавление компонентов

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const FILM_PROMO = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  released: '2014',
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      name = {FILM_PROMO.name}
+      genre = {FILM_PROMO.genre}
+      released = {FILM_PROMO.released}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
